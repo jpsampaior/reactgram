a2b118f44e5e4ff28f0d9bb4fec59f7ff2b38850
João Pedro Sampaio Ribeiro <jpsampaio@edu.unifor.br>
2024-07-04 18:51:30 -0300

    :tada: init frontend

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import './App.css';
+
+import Home from './pages/Home/Home';
+import Login from './pages/Auth/Login';
+import Register from './pages/Auth/Register';
+
+// Router
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home/>} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/register' element={<Register />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
